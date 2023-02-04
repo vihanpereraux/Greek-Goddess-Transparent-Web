014e415edc1387dca154cdc11ee0e6a430180a57
Vihan Perera <vihanpereraux@gmail.com>
2023-02-04 21:38:33 +0530

    Rename CSS selectors

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,10 @@
 import LocomotiveScroll from 'locomotive-scroll';
+// import gsap from 'gsap'z;
+// import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
     lerp: 0.05
 });
+
