4c5e7ea1a8a92864e9cc483d7e6b343ede6529e3
Vihan Perera <vihanpereraux@gmail.com>
2023-02-05 13:45:22 +0530

    Add image animations

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,6 +2,7 @@ import LocomotiveScroll from 'locomotive-scroll';
 // import gsap from 'gsap'z;
 // import ScrollTrigger from 'gsap/ScrollTrigger';
 
+
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
@@ -9,3 +10,21 @@ const scroll = new LocomotiveScroll({
     multiplier: .5
 });
 
+
+// scrollmagic init
+let ctrl = new ScrollMagic.Controller();
+
+// each image
+[].forEach.call(document.querySelectorAll('.inner_img'), function (e) {
+
+    // gsap timeline
+    let tl = new TimelineMax();
+    tl.from(e, 1, { xPercent: -100, ease: Expo.easeInOut, ease: 'power2.in' }, 0);
+    tl.from(e.querySelector('.img'), 1, { xPercent: 100, ease: Expo.easeInOut, ease: 'power2.in' }, 0);
+
+    // scrollmagic scene
+    let scene = new ScrollMagic.Scene({ triggerElement: e, triggerHook: .6 })
+        .setTween(tl)
+        .addTo(ctrl);
+
+});
